8c90d9aa441cdf7e5d05458d7c4d3047a192a72e
joseph khella <josephkhella2010@gmail.com>
2026-05-18 15:49:02 +0200

    add test to backend

---
+++ backend/test/getUser.test.js
@@ -0,0 +1,30 @@
+import request from "supertest";
+import app from "../server.js";
+
+describe("Users API", () => {
+  test("GET users returns 200", async () => {
+    const res = await request(app).get("/api/users");
+    expect(res.statusCode).toBe(200);
+  });
+
+  test("GET users returns object with users array", async () => {
+    const res = await request(app).get("/api/users");
+
+    expect(res.body).toHaveProperty("users");
+    expect(Array.isArray(res.body.users)).toBe(true);
+  });
+
+  test("GET users is not empty", async () => {
+    const res = await request(app).get("/api/users");
+
+    expect(res.body.users.length).toBeGreaterThan(0);
+  });
+
+  test("GET users contains Emby does NOT apply here", async () => {
+    const res = await request(app).get("/api/users");
+
+    const hasUser = res.body.users.some((user) => user.username === "ali01");
+
+    expect(hasUser).toBe(true);
+  });
+});
