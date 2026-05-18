89eebe5fb46ee66cb69cac3032c92d4f4efd4912
joseph khella <josephkhella2010@gmail.com>
2026-05-18 20:14:59 +0200

    add home and route page

--- frontend/trainTwo/src/App.tsx
+++ frontend/trainTwo/src/App.tsx
@@ -1,12 +1,10 @@
 import "./App.css";
+import RoutePage from "./Routes/RoutePage";
 
 function App() {
-  const name = "Jesus Christ";
   return (
     <>
-      <h1> hellow world </h1>
-      <h1> Hej san Hur ar det</h1>
-      <h1>Hi {name} </h1>
+      <RoutePage />
     </>
   );
 }
