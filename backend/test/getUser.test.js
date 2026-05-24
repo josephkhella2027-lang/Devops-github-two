04da1375f290721cdc91418a19f0bc7302b6336d
joseph khella <josephkhella2010@gmail.com>
2026-05-24 19:22:12 +0200

    fix error three

--- backend/test/getUser.test.js
+++ backend/test/getUser.test.js
@@ -22,7 +22,7 @@ describe("Users API", () => {
     const res = await request(app).get("/api/users");
 
     expect(res.body).toHaveProperty("message");
-    expect(typeof res.body.message).toBe("array");
+    expect(Array.isArray(res.body.message)).toBe(true);
   });
 
   test("Each user has correct properties", async () => {
